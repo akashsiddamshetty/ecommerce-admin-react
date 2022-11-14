67cd4b5e7d426e0b6e1f15f6b68491d65ed3fa01
ktk <akashsiddamshetty@gmail.com>
2022-11-14 21:50:38 +0530

    -- added charts -- added bars

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,9 +1,69 @@
-import './chart.scss'
+import "./chart.scss";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  {
+    name: "January",
+    Total: 1200,
+  },
+  {
+    name: "February",
+    Total: 2100,
+  },
+  {
+    name: "March",
+    Total: 800,
+  },
+  {
+    name: "April",
+    Total: 1600,
+  },
+  {
+    name: "May",
+    Total: 900,
+  },
+  {
+    name: "June",
+    Total: 1700,
+  },
+];
 
 const Chart = () => {
   return (
-    <div className='chart'>Chart</div>
-  )
-}
+    <div className="chart">
+      <div className="title">Last 6 Months (Revenue)</div>
+      <ResponsiveContainer width="100%" height={320}>
+        <AreaChart
+          width={500}
+          height={400}
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" className="chartGrid" />
+          <XAxis dataKey="name" stroke="gray" />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="Total"
+            stroke="#8884d8"
+            fill="#8884d8"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
 
-export default Chart
+export default Chart;
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,7 +1,51 @@
 import "./featured.scss";
-
+import MoreVertTwoToneIcon from "@mui/icons-material/MoreVertTwoTone";
+import { CircularProgressbar } from "react-circular-progressbar";
+import KeyboardArrowUpTwoToneIcon from "@mui/icons-material/KeyboardArrowUpTwoTone";
+import KeyboardArrowDownTwoToneIcon from "@mui/icons-material/KeyboardArrowDownTwoTone";
+import "react-circular-progressbar/dist/styles.css";
 const Featured = () => {
-  return <div className="featured">Featured</div>;
+  return (
+    <div className="featured">
+      <div className="top">
+        <h1 className="title">Total Revenue</h1>
+        <MoreVertTwoToneIcon className="icon " />
+      </div>
+      <div className="bottom">
+        <div className="featuredChart">
+          <CircularProgressbar value={70} text={"70%"} strokeWidth={5} />
+        </div>
+        <p className="title">Total sales made today</p>
+        <p className="amount">$420</p>
+        <p className="desc">
+          Previous transactions processing. Last payments may not be included
+        </p>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpTwoToneIcon className="icon" />
+              <div className="resultAmount">$12k</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last Week</div>
+            <div className="itemResult negative">
+              <KeyboardArrowDownTwoToneIcon className="icon" />
+              <div className="resultAmount">$12k</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last Month</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpTwoToneIcon className="icon" />
+              <div className="resultAmount">$12.4 k</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
